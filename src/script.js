7b66e023d018a6b0605276cfd742b4337e26af23
Arthur <arthur@corenzan.com>
2019-02-02 14:27:51 -0200

    Fixes and tweaks

--- src/script.js
+++ src/script.js
@@ -3,7 +3,6 @@ function getElementLocation(target) {
     target,
     parent: target.parentElement,
     index: -1
-    // depth: -1
   };
 
   let sibling = target;
@@ -12,51 +11,45 @@ function getElementLocation(target) {
     location.index += 1;
   } while (sibling);
 
-  //   let parent = target;
-  //   do {
-  //     parent = parent.parentElement;
-  //     location.depth += 1;
-  //   } while (parent);
-
   return location;
 }
 
-// function compareLocationDepth(a, b) {
-//   return a.depth < b.depth ? -1 : a.depth === b.depth ? 0 : 1;
-// }
-
-function reattachElements(locations) {
-  //   locations.sort(compareLocationDepth);
-
-  for (let i = locations.length; i > 0; i--) {
-    const loc = locations.pop();
-    loc.parent.insertBefore(loc.target, loc.parent.children[loc.index]);
-  }
+function reattachElement(location) {
+  location.parent.insertBefore(
+    location.target,
+    location.parent.children[location.index]
+  );
 }
 
 function detachElement(target) {
-  const loc = getElementLocation(target);
-  loc.parent.removeChild(target);
-  return loc;
+  const location = getElementLocation(target);
+  location.parent.removeChild(target);
+  return location;
 }
 
-const removedLocations = [];
+const savedLocations = [];
 
 function changeLanguage(lang) {
-  if (removedLocations.length > 0) {
-    reattachElements(removedLocations);
+  while (savedLocations.length > 0) {
+    reattachElement(savedLocations.pop());
   }
   document.firstElementChild.setAttribute("lang", lang);
   const elements = document.querySelectorAll(`[lang]`);
   [].forEach.call(elements, element => {
     if (element.getAttribute("lang") != lang) {
-      removedLocations.push(detachElement(element));
+      savedLocations.push(detachElement(element));
     }
   });
 }
+window.changeLanguage = changeLanguage;
 
 document.addEventListener("DOMContentLoaded", e => {
-  changeLanguage(document.firstElementChild.getAttribute("lang"));
+  switch (navigator.language) {
+    case "pt":
+    case "pt-BR":
+      changeLanguage("pt-BR");
+      break;
+    default:
+      changeLanguage("en");
+  }
 });
-
-window.changeLanguage = changeLanguage;
