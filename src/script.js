c8628aaff6b52092208a8327d59ba399d2230b3b
Arthur <arthur@corenzan.com>
2019-02-04 10:01:45 -0200

    Refactor a little of the translation code

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
-function getElementLocation(target) {
+function getElementLocation(element) {
   const location = {
-    target,
-    parent: target.parentElement,
+    element,
+    parent: element.parentElement,
     index: -1
   };
 
-  let sibling = target;
+  let sibling = element;
   do {
     sibling = sibling.previousElementSibling;
     location.index += 1;
@@ -15,42 +15,58 @@ function getElementLocation(target) {
 }
 
 function reattachElement(location) {
+  if (!location.parent.parentElement) {
+    throw Error(
+      "reattachElement: Parent element is no longer present in the DOM tree"
+    );
+  }
   location.parent.insertBefore(
-    location.target,
+    location.element,
     location.parent.children[location.index]
   );
 }
 
-function detachElement(target) {
-  const location = getElementLocation(target);
-  location.parent.removeChild(target);
+function detachElement(element) {
+  const location = getElementLocation(element);
+  location.parent.removeChild(element);
   return location;
 }
 
-const savedLocations = [];
+const detachedElements = [];
 
 function changeLanguage(lang) {
-  while (savedLocations.length > 0) {
-    reattachElement(savedLocations.pop());
+  while (detachedElements.length > 0) {
+    reattachElement(detachedElements.pop());
   }
-  document.firstElementChild.setAttribute("lang", lang);
+  document.documentElement.setAttribute("lang", lang);
   const elements = document.querySelectorAll(`[lang]`);
   [].forEach.call(elements, element => {
     if (element.getAttribute("lang") != lang) {
-      savedLocations.push(detachElement(element));
+      detachedElements.push(detachElement(element));
     }
   });
 }
 window.changeLanguage = changeLanguage;
 
 document.addEventListener("DOMContentLoaded", e => {
-  switch (navigator.language) {
-    case "pt":
-    case "pt-BR":
-      changeLanguage("pt-BR");
-      break;
-    default:
-      changeLanguage("en");
+  let selectedLang = document.documentElement.lang;
+
+  if (navigator.language) {
+    let sample = document.querySelector(`[lang="${navigator.language}"]`);
+    if (!sample) {
+      const prefix = navigator.language.split("-")[0];
+      sample = document.querySelector(`[lang|="${prefix}"]`);
+    }
+    if (sample) {
+      selectedLang = sample.lang;
+      console.log("Language derived from navigator.language:", sample.lang);
+    }
+  }
+
+  if (selectedLang) {
+    changeLanguage(selectedLang);
+  } else {
+    throw Error("Attribute [lang] is missing from the documentElement");
   }
 });
 
